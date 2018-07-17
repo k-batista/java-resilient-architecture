4eafdc9e61fc01e40f488bf61bd5f0fb3489c6be
rmacario <rafael.m.macario@gmail.com>
2018-07-16 22:36:34 -0300

    Initial commit - cars-storage-api

---
+++ cars-storage-api/src/test/java/br/com/rmacario/carsstorageapi/CarsStorageApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.rmacario.carsstorageapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarsStorageApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
